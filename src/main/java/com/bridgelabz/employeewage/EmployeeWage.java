1d0b71f00e2d6ef836e4c68f9599a6a1666adc52
vijikumar123 <vk9164940756@gmail.com>
2023-01-24 13:17:25 +0530

    added: uc9 EmployeeWage problems

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,36 +3,43 @@ package com.bridgelabz.employeewage;
 import java.util.Scanner;
 
 public class EmployeeWage {
+    static int  wagePerHour ;
+    static int workingHoursPerMonth ;
+    static int workingDaysPerMonth ;
+    static int count = 0;
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Problem");
 
+        System.out.println("Welcome to employee wage");
+        EmployeeWage obj = new EmployeeWage();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your count of company's");
-
         int company = sc.nextInt();
 
         for ( int i = 1 ; i<=company ; i++ ){
             System.out.println("Enter wage per hour ");
-            int wagePerHour = sc.nextInt();
+            wagePerHour = sc.nextInt();
             System.out.println("Enter workingHoursPerMonth ");
-            int workingHoursPerMonth = sc.nextInt();
+            workingHoursPerMonth = sc.nextInt();
             System.out.println("Enter workingDaysPerMonth ");
-            int workingDaysPerMonth = sc .nextInt();
-            wageCalculation();
+            workingDaysPerMonth = sc .nextInt();
+            obj.refactor();
+
         }
+
+
+
     }
-    public static void wageCalculation() {
-        int wagePerHour;
-        int workingHoursPerMonth = 0;
-        int workingDaysPerMonth = 0;
+    public void refactor(){
+
         int fullDayHour = 8;
         int partTimeHour = 4;
         int isFullTime = 2;
         int isPartTime = 1;
         int totalWage = 0;
+
         int dayCount = 1;
         int workingHours = 0;
-        while (dayCount < workingDaysPerMonth && workingHours < workingHoursPerMonth) {
+        while (dayCount <workingDaysPerMonth && workingHours < workingHoursPerMonth ) {
             int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
             System.out.println(employeeCheck);
             int dailyWage = 0;
@@ -40,24 +47,27 @@ public class EmployeeWage {
                 case 1:
                     dailyWage = partTimeHour * wagePerHour;
                     workingHours += partTimeHour;
-                    System.out.println("Employee is part time " + dailyWage);
+                    System.out.println("Employee is part time " +dailyWage);
                     break;
                 case 2:
                     dailyWage = fullDayHour * wagePerHour;
-                    workingHours += fullDayHour;
-                    System.out.println("Employee is full time " + dailyWage);
+                    workingHours +=fullDayHour;
+                    System.out.println("Employee is full time " +dailyWage);
                     break;
                 default:
                     System.out.println("Employee is Absent");
                     break;
             }
             dayCount++;
-            totalWage = totalWage + dailyWage;
+            totalWage = totalWage +dailyWage;
         }
-        System.out.println("Total hours " + workingHours);
-        System.out.println(" totalWage wage for month " + totalWage);
+        count ++;
+        System.out.println("Total hours "+ workingHours);
+        System.out.println(" totalWage wage for month company" + count +" "+ totalWage);
+
     }
-    }
+}
+
 
 
 
