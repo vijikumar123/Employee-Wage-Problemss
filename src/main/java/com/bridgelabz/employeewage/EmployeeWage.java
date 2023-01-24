466b8f217afe6eea6d3b2564cd4b8cef03b135b9
vijikumar123 <vk9164940756@gmail.com>
2023-01-24 13:00:52 +0530

    added: uc5 EmployeeWage problems

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,10 +3,10 @@ package com.bridgelabz.employeewage;
 import java.util.Scanner;
 
 public class EmployeeWage {
+    // UC-1------
     void checkEmployeeAttendence() {
         int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-
+        double empCheck = (int)(Math.random() * 2) ;
         if (empCheck == IS_FULL_TIME)
             System.out.println("Employee is Present");
         else
@@ -30,7 +30,7 @@ public class EmployeeWage {
         System.out.println("Employee Wage " + employeeWage);
     }
 
-    //UC-3----------------------------
+    //UC-3-----------------------------
     void dailyEmployeePartTimeWage() {
         int fullTime = 2;
         int partTime = 1;
@@ -72,13 +72,45 @@ public class EmployeeWage {
         return employeeWage;
 
     }
+
+    // UC-5----------------
+    void employeeMonthlyWage() {
+        int fullTime = 2;
+        int partTime = 1;
+        int employeeWagePerHour = 20;
+        int workingDaysPerMonth = 20;
+        int employeeHrs = 0;
+        int employeeWage = 0;
+        int monthlyWage = 0;
+        for (int day = 0; day < workingDaysPerMonth; day++) {
+            int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (employeeCheck) {
+                case 1:
+                    employeeHrs = 16;
+                    break;
+                case 2:
+                    employeeHrs = 8;
+                    break;
+                default:
+                    employeeHrs = 0;
+
+            }
+            employeeWage = employeeHrs * employeeWagePerHour;
+            monthlyWage += employeeWage;
+            System.out.println("Employee wage:" + employeeWage);
+        }
+        System.out.println("Total Employee Wage " + monthlyWage);
+    }
+
+
+
     public static void main(String[] args) {
         EmployeeWage Emp = new EmployeeWage();
          Emp.checkEmployeeAttendence();
-         Emp.dailyEmployeeWage();
+        //Emp.dailyEmployeeWage();
         // Emp.dailyEmployeePartTimeWage();
-
-        System.out.println(Emp.switchcase());
+        Emp.employeeMonthlyWage();
+        // System.out.println(Emp.switchcase());
 
     }
 
